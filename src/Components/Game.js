aa90462ff988d132794578972d7835336e900f2d
Charlie Moran <Carlosamoran18@gmail.com>
2022-05-10 11:22:56 -0400

    catching up

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,23 +1,29 @@
-import { Component } from "react";
-import Square from "./Square";
-import "./Game.css";
+import { Component } from 'react';
+import Square from './Square';
+import './Game.css';
 
-const board = ["X", null, null, null, "O", "O", null, null, "X"];
+const board = ['X', null, null, null, 'O', 'O', null, null, 'X'];
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      board: ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    };
+  }
   render() {
+    const squares = board.map((value, index) => (
+      <Square key={index} value={value} />
+    ));
+
     return (
-      <div className="tic-tac-toe-game">
+      <div className='tic-tac-toe-game'>
         <h1>Tic Tac Toe</h1>
-        <div className="Board">
-          {/* RENDER NINE SQUARE COMPONENTS HERE.
-          PASS A PROP CALLED "value" THAT DETERMINES
-          WHETHER THE SQUARE DISPLAYS NOTHING, AN X,
-          OR AN O BASED ON THE VALUES IN board */}
-        </div>
+        <div className='Board'>{this.state.squares}</div>
       </div>
     );
   }
 }
 
 export default Game;
+
