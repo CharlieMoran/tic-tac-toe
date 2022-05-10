f302dd0517fbf9ef9c1cfea28cdfc3c5f49c5a0d
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-05-10 08:54:55 -0400

    adds board to state

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -2,11 +2,18 @@ import { Component } from "react";
 import Square from "./Square";
 import "./Game.css";
 
-const board = ["X", null, null, null, "O", "O", null, null, "X"];
-
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      board: ["X", null, null, null, "O", "O", null, null, "X"],
+    };
+  }
+
   render() {
-    const squares = board.map((val, ind) => <Square key={ind} value={val} />);
+    const squares = this.state.board.map((val, ind) => (
+      <Square key={ind} value={val} />
+    ));
     return (
       <div className="tic-tac-toe-game">
         <h1>Tic Tac Toe</h1>
